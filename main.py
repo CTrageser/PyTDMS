c3a4b7404b6f668a5a60c018e707b274cf2f2635
CTrageser <c.trageser@gsi.de>
2015-05-26 20:51:33 +0200

    Create main.py
    this file contains a small example to use the PyTDMS project

---
+++ main.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------------------
+# File.:        main.py
+# Date.:        May 26th, 2015
+# Author.:      C.Trageser
+# Description.: This file shows how to use PyTDMS classes in a small example.
+# --------------------------------------------------------------------------------------------------------
+
+print ("INFO \t Starting example ...")
+print ("INFO \t exiting example ...")
